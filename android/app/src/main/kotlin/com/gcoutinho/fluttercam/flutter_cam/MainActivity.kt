777da9d77d44542018a4ec12e68f23fcd388aabb
Guilherme Coutinho <guilhermecoutinho7777@gmail.com>
2021-12-06 15:59:35 -0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/gcoutinho/fluttercam/flutter_cam/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.gcoutinho.fluttercam.flutter_cam
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_cam/home_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+
